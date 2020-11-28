582d70f7bb83d59ec61b2cc6f35d567dd2bdb7e3
Marco Ares <39780326+jmarcoares98@users.noreply.github.com>
2020-11-28 13:38:03 -0800

    Added test script for CI/CD

---
+++ test.sh
@@ -0,0 +1,23 @@
+  #!/bin/sh
+  echo "here are test jobs for ChemNetworks-2.2"
+  for i in {1..4}
+  do
+    cp ChemNetworks-2.2.exe test-jobs/test$i/ChemNetworks-2.2.exe
+  done
+  
+  cd test-jobs
+  for task in {1..4}
+  do
+  case $task in
+    1) ARGS="water.xyz" ;;
+    2) ARGS="water1.xyz solB1.xyz" ;;
+    3) ARGS="water1.xyz solB1.xyz solC1.xyz" ;;
+    4) ARGS="water1.xyz solC1 solB1.xyz" ;;
+  esac
+  pushd test$task
+  echo "test$task..."
+  ./chemNetworks-2.2.exe Input-test$task $ARGS
+  fi
+  popd
+  done
+  wait
