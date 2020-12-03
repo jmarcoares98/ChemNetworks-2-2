770fbc12a1477b0ef701f592b5539dcfe8768145
Marco Ares <39780326+jmarcoares98@users.noreply.github.com>
2020-12-02 21:20:53 -0800

    Added input variables

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
-  #!/bin/sh
+  #!/usr/bin/env bash
+  ARG1=${1?Error: no input given}
+  ARG2=${2?Error: no input given}
+  ARG3=${3?Error: no input given}
+  
   echo "here are test jobs for ChemNetworks-2.2"
  
   for ((i=1; ;++i))
@@ -15,10 +19,15 @@
   cd test-jobs
   for ((task=1; ;++task))
   do
+  case $task in
+  1) ARGS="$ARG1" ;;
+  2) ARGS="$ARG1 $ARG2" ;;
+  3) ARGS="$ARG1 $ARG2 $ARG3" ;;
+  4) ARGS="$ARG1 $ARG3 $ARG2" ;;
+  esac
   pushd test$task
   echo "$?"
   echo "test$task..."
-  read -p "Enter inputs : " ARGS
   chmod +x ChemNetworks-2.2.exe
   ./ChemNetworks-2.2.exe Input-test$task $ARGS
   lab=$?
