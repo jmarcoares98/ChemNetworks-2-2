477da317da49725875da180252d9d0751705afe6
Marco Ares <39780326+jmarcoares98@users.noreply.github.com>
2020-12-02 20:42:17 -0800

    Added user input

--- test.sh
+++ test.sh
@@ -15,17 +15,18 @@
   cd test-jobs
   for ((task=1; ;++task))
   do
-  read -p "Enter inputs : " ARGS
   pushd test$task
+  echo "$?"
   echo "test$task..."
+  read -p "Enter inputs : " ARGS
   chmod +x ChemNetworks-2.2.exe
   ./ChemNetworks-2.2.exe Input-test$task $ARGS
   lab=$?
-  echo ""
   if test $lab -eq 0; then
     echo "test$task passed"
   else 
     break
   fi
+  echo""
   popd
   done
